349c13ead6f1e3a878cb2b71a0ecf6e8c571a5f4
profnsi <>
2020-07-12 16:05:08 +0000

    retour au nom correct

--- var/www/Projet_ISN_/config/jupyter_prof/nbgrader_config.py
+++
@@ -1,9 +0,0 @@
-c = get_config()
-c.CourseDirectory.root = '/home/manu/ipynb'
-
-#c.CourseDirectory.course_id = "ipynb"
-#c.IncludeHeaderFooter.header = "source/header.ipynb"
-c.ClearSolutions.code_stub = {'python': '# TAPEZ VOTRE CODE ICI', 'matlab': "% YOUR CODE HERE\nerror('No Answer Given!')", 'octave': "% YOUR CODE HERE\nerror('No Answer Given!')", 'java': '// YOUR CODE HERE'}
-c.ClearSolutions.text_stub = 'VOTRE REPONSE'
-
-
