ea0fe0c6d7f0d6becc1a9ec9e73aaedde47149ac
manu <>
2020-11-14 22:24:07 +0100

    erreurs de chemins

--- www/site_nsi/cli/error_php.sh
+++ www/site_nsi/cli/error_php.sh
@@ -3,8 +3,8 @@
 if [ "$UID" -ne "0" ]
 then
   echo "Il faut etre root pour executer ce script. ==> sudo "
-  exit 
-fi 
+  exit
+fi
 
 if [ "$1" != "on" ] && [ "$1" != "off" ] ; then
   echo "usage $0 on|off"
@@ -16,12 +16,11 @@ php_ini="/etc/php/${php_ver}/fpm/php.ini"
 if [ "$1" == "on" ] ; then
     sed -i -e "s/error_reporting = E_ALL \& ~E_DEPRECATED \& ~E_STRICT$/error_reporting = E_ALL/" "$php_ini"
     sed -i -e "s/display_errors = Off/display_errors = On/" "$php_ini"
-    sed -i -e "s/\/\/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/" /var/www/Projet_ISN/src/models/Database.php
+    sed -i -e "s/\/\/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/" /var/www/site_nsi/src/models/Database.php
 else
     sed -i -e "s/error_reporting = E_ALL$/error_reporting = E_ALL \& ~E_DEPRECATED \& ~E_STRICT/" "$php_ini"
     sed -i -e "s/display_errors = On/display_errors = Off/" "$php_ini"
-    sed -i -e "s/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/\/\/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/" /var/www/Projet_ISN/src/models/Database.php
+    sed -i -e "s/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/\/\/\$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION)/" /var/www/site_nsi/src/models/Database.php
 fi
 systemctl restart php${php_ver}-fpm.service
 echo "affichage des erreurs php $1"
-
