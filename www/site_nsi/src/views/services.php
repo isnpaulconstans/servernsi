2de8298ea466c0ebadad4c00571fe32754878dfd
manu <>
2020-11-16 14:12:38 +0100

    faute d'orthographe

--- www/site_nsi/src/views/services.php
+++ www/site_nsi/src/views/services.php
@@ -5,7 +5,8 @@
         </h2>
         <p>
             Jirafeau est un service de partage de fichiers libre et open source.<br>
-            Il vous suffit d'importer votre fichier, de selectionner une durée et de partager le lien avec qui vous souhaitez.<br>
+            Il vous suffit d'importer votre fichier, de sélectionner une durée et
+            de partager le lien avec qui vous souhaitez.<br>
             Il est possible de protéger l'accès au fichier avec un mot de passe.
         </p>
         <a class="button" href="/jirafeau">Le service</a>
@@ -19,8 +20,10 @@
             Gitea
         </h2>
         <p>
-            Gitea est un logiciel permettant le suivi du développement de projets collaboratifs.<br>
-            Le système de validation rend possible la visualisation des différentes versions.
+            Gitea est un logiciel permettant le suivi du développement de projets
+             collaboratifs.<br>
+            Le système de validation rend possible la visualisation des différentes
+             versions.
         </p>
         <a class="button" href="/gitea">Le service</a>
     </div>
